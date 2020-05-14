19e5f754619182232637980d1f694867a3ce7fa7
Israel Canessa <israellcanessa@gmail.com>
2020-05-14 05:24:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,14 +4,14 @@ def position_taken?(board, index)
 end
 
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+win_index_1 = win_combination[0]
+win_index_2 = win_combination[1]
+win_index_3 = win_combination[2]
+position_1 = board[win_index_1]
+position_2 = board[win_index_2]
+position_3 = board[win_index_3]
 
 def move?(board)
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
   if position_1 == "X" && position_2 == "X" && position_3 =="X"
     return win_combination
   else
