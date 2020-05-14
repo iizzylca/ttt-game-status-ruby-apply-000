890635e0fafc1d60cfb19c2ab41b832ea32b997e
Israel Canessa <israellcanessa@gmail.com>
2020-05-14 07:03:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,44 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    location_1 = win_combination[0]
+    location_2 = win_combination[1]
+    location_3 = win_combination[2]
+    board[location_1] == board[location_2] && board[location_2] == board[location_3] && board[location_1] != " "
+  end
+end
+
+def full?(board)
+  board.none? do |position|
+    position == " "
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) && !draw?(board) || draw?(board)
+end
+
+def winner(board)
+  win_combination = won?(board)
+  if win_combination
+    win_location = win_combination[0]
+    board[win_location]
+  end
+end
