e8b4bd60100bc3f06cf8842ec09e9a03043a9342
Israel Canessa <israellcanessa@gmail.com>
2020-05-14 05:50:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,6 +13,7 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
 def move?(board)
   WIN_COMBINATIONS.detect do |win_combination|
     board[0] == board[1] && board[1] == board[2] && board[0] != " "
